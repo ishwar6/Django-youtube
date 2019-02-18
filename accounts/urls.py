f815c6425be3c484d1a5650efb2033cc82abc6a0
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2019-02-18 18:01:14 +0530

    full register urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import url, include
 from .views import (
-    send_otp_this, otp_match
+    send_otp_this, otp_match, register, create
 )
 
 app_name = 'accounts'
 urlpatterns = [
-
+    url(r'^start/$', register, name='register'),
 
     url(r'^ajax/new/$', send_otp_this, name='new'),
 
     url(r'^ajax/match/$', otp_match, name='match'),
+    url(r'^ajax/create/$', create, name='create'),
     # url(r'^ajax/validate_otp/reset/$',
     #     validate_otp_reset, name='validate-otp-reset'),
     # url(r'^ajax/validate_phone/reset/$',
