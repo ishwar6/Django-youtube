a940447aa8cea4e3ffb80dfcb71836d30922440d
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2019-02-20 17:52:40 +0530

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,3 +5,8 @@ class OTPForm(forms.Form):
     phone = forms.IntegerField()
     otp = forms.IntegerField()
     password = forms.CharField()
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
