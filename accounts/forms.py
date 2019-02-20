8045bca3f28ae0240ec44917070463590a3168ba
Ishwar Jangid <ishwarjangid@Ishwars-MacBook-Air.local>
2019-02-20 22:12:06 +0530

    Changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,3 +4,9 @@ from django import forms
 class OTPForm(forms.Form):
     phone = forms.IntegerField()
     otp = forms.IntegerField()
+    password = forms.CharField()
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import PhoneOTP
-from .forms import OTPForm
+from .forms import OTPForm, LoginForm
 import random
 from django.http import JsonResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 import requests
+from django.contrib.auth import get_user_model, login, logout, authenticate, login
+User = get_user_model()
+
+
+def register(request):
+    form = OTPForm()
+    return render(request, 'accounts/phone.html', {'form': form})
 
 
 def send_otp_phone(phone, key):
@@ -13,12 +20,16 @@ def send_otp_phone(phone, key):
 
     link = f'https://2factor.in/API/R1/?module=TRANS_SMS&apikey=YOUR_API_KEY&to={phone}&from=YOUR_TEMPLATE&templatename=YOUR_TEMPLATE_NAME&var1={name}&var2={otp_key}'
 
-    result = requests.get(link, verify=False)
+    #result = requests.get(link, verify=False)
 
 
 def send_otp_this(request):
     phone = request.GET.get('phone')
     print(phone)
+    phone_obj = User.objects.filter(username=phone)
+    if phone_obj.exists():
+        return JsonResponse({'send': False, 'taken': True})
+
     key = random.randint(999, 9999)
     print(key)
 
@@ -63,3 +74,40 @@ def otp_match(request):
             return JsonResponse({'match': False, 'msg': True})
     else:
         return JsonResponse({'match': False, 'msg': True})
+
+
+def create(request):
+    phone = request.GET.get('phone')
+    password = request.GET.get('pass')
+    if phone and password:
+        user = User.objects.create_user(
+            username=phone,
+            password=password
+        )
+        login(request, user)
+        return JsonResponse({'create': True})
+    else:
+        return JsonResponse({'create': False})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('post-show')
+
+
+def login_view(request):
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username', None)
+            password = form.cleaned_data.get('password', None)
+            if username and password:
+                user_obj = authenticate(
+                    request, username=username, password=password)
+                if user_obj is not None:
+                    login(request, user_obj)
+                    return redirect('post-show')
+
+    return render(request, 'accounts/login.html', {'form': form})
