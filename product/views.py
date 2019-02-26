7cd1a1f3775cb36f18b97d14e78955eab5a97ae7
ishwar6 <ishawarjangid66@gmail.com>
2019-02-26 16:12:58 +0530

    cart views

--- product/views.py
+++ product/views.py
@@ -13,7 +13,9 @@ class ProductList(ListView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        cart_obj = Cart.objects.filter(user=self.request.user).first()
-        product_added = cart_obj.product.all()
-        context['product_added'] = product_added
+        cart_obj = Cart.objects.filter(user=self.request.user)
+        if cart_obj.exists():
+            cart_obj = cart_obj.last()
+            product_added = cart_obj.product.all()
+            context['product_added'] = product_added
         return context
