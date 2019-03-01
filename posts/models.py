ecf1659211b353a23454258072e397d97b808173
ishwar6 <ishawarjangid66@gmail.com>
2019-03-01 08:40:57 +0530

    change

--- posts/models.py
+++ posts/models.py
@@ -16,9 +16,6 @@ class Subject(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolute_url(self):
-        return '/subject/id'
-
 
 STATUS_CHOICES = (
     ('draft', 'Draft'),
