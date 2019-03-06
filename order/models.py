73c221ec97e57bdafb66a8d05187a2c7b3e33f78
ishwar6 <ishawarjangid66@gmail.com>
2019-03-06 09:36:32 +0530

    order Model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+ORDER_STATUS_CHOICES = (
+    ('created', 'Created'),
+    ('paid', 'Paid'),
+    ('shipped', 'Shipped'),
+    ('refunded', 'Refunded'),
+)
+
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=120, blank=True)
+
+    billing_address = models.CharField(null=True, blank=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
+    shipping_total = models.DecimalField(
+        default=5.99, max_digits=10, decimal_places=2)
+    total = models.DecimalField(default=0.00, max_digits=10, decimal_places=2)
+    active = models.BooleanField(default=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp', '-updated']
+
+    def __str__(self):
+        return self.order_id
+
+    def get_status(self):
+
+        if self.status == "created":
+            return 'Created'
+        if self.status == "refunded":
+            return "Refunded order"
+        elif self.status == "shipped":
+            return "Shipped"
+        return "Shipping Soon"
