744e962a52662fe2e4d92864a38c826ffb64969d
ishwar6 <ishawarjangid66@gmail.com>
2019-03-17 15:18:33 +0530

    model

--- posts/models.py
+++ posts/models.py
@@ -35,6 +35,7 @@ def upload_image_path_posts(instance, filename):
 
 
 def get_filename_ext(filepath):
+
     base_name = os.path.basename(filepath)
     name, ext = os.path.splitext(base_name)
     return name, ext
